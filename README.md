e09bac8d5cf0bf5b24da10f157c407707359e953
Isaac Azuelos <isaac@azuelos.ca>
2018-12-01 16:25:21 -0700

    Cleanup, since I'm at a real computer.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # aoc2018
+
+Code for the 2018 [Advent of Code][aoc].
+
+[aoc]: http://adventofcode.com/
--- day01/day01.py
+++ day01/day01.py
@@ -1,18 +1,17 @@
 from itertools import cycle
 
-lines = open('input1.txt', 'r').readlines()
-print(sum(int(l) for l in lines))
+lines = open("input.txt", "r").readlines()
 nums = list(int(l) for l in lines)
+
+print(f"part 1: {sum(nums)}")
 
 seen = set()
 acc = 0
 
 for n in cycle(nums):
     seen.add(acc)
-    acc+=n
+    acc += n
     if acc in seen:
-        print(acc)
         break
 
-    
-#print("acc= ", acc, "seen=", seen)
+print(f"part 2: {acc}")
